6ee284f8d070453354d2ea91d184b8f0458e75aa
Kottage N.V.H <87430226+KOTTAGENVH@users.noreply.github.com>
2024-08-31 23:37:21 +0530

    Update layout.tsx

--- mic-talk/src/app/layout.tsx
+++ mic-talk/src/app/layout.tsx
@@ -8,7 +8,7 @@ import { SpeakerProvider } from "@/contextApi/speakerContext";
 import { VolumeProvider } from "@/contextApi/volumeContext";
 
 const inter = Inter({ subsets: ["latin"] });
-const adClient = process.env.AD_CLIENT as string;
+
 export const metadata: Metadata = {
   title: "Mic-Talk",
   description: "Mic Talk by Nowen Kottage (`www.nowenkottage.com`)",
@@ -19,7 +19,6 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const source = process.env.ADSTERRA_SRC as string;
   return (
     <html lang="en">
       <body className={inter.className}>
