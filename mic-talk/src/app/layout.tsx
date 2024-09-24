85cf7691a1effb7919766b3d405ec090bb575ecb
Kottage N.V.H <87430226+KOTTAGENVH@users.noreply.github.com>
2024-09-24 23:46:11 +0530

    Update layout.tsx

--- mic-talk/src/app/layout.tsx
+++ mic-talk/src/app/layout.tsx
@@ -1,13 +1,19 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 import { ThemeProvider } from "@/contextApi/darkmodeContext";
 import { MicrophoneProvider } from "@/contextApi/microphoneContext";
 import { ModalProvider } from "@/contextApi/modalContext";
 import { SpeakerProvider } from "@/contextApi/speakerContext";
 import { VolumeProvider } from "@/contextApi/volumeContext";
 import Script from "next/script";
-import { Inter } from "next/font/google";
-import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Mic-Talk",
+  description: "Mic Talk by Nowen Kottage (`www.nowenkottage.com`)",
+};
 
 export default function RootLayout({
   children,
