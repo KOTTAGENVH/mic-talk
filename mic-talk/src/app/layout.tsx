f41a52b37a8819a3a5f32004ebe3fd3ba3c6de04
Kottage N.V.H <87430226+KOTTAGENVH@users.noreply.github.com>
2024-09-24 23:45:00 +0530

    fix: adsterra issue

--- mic-talk/src/app/layout.tsx
+++ mic-talk/src/app/layout.tsx
@@ -1,19 +1,13 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
 import { ThemeProvider } from "@/contextApi/darkmodeContext";
 import { MicrophoneProvider } from "@/contextApi/microphoneContext";
 import { ModalProvider } from "@/contextApi/modalContext";
 import { SpeakerProvider } from "@/contextApi/speakerContext";
 import { VolumeProvider } from "@/contextApi/volumeContext";
 import Script from "next/script";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Mic-Talk",
-  description: "Mic Talk by Nowen Kottage (`www.nowenkottage.com`)",
-};
 
 export default function RootLayout({
   children,
@@ -21,22 +15,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <VolumeProvider>
-          <SpeakerProvider>
-            <ModalProvider>
-              <MicrophoneProvider>
-                <ThemeProvider>{children}</ThemeProvider>
-              </MicrophoneProvider>
-            </ModalProvider>
-          </SpeakerProvider>
-        </VolumeProvider>
-        <Script
-            src={process.env.NEXT_PUBLIC_ADSTERRA_SRC}
-            strategy="lazyOnload"
-          />
-      </body>
-    </html>
+    <div className={inter.className}>
+      <VolumeProvider>
+        <SpeakerProvider>
+          <ModalProvider>
+            <MicrophoneProvider>
+              <ThemeProvider>
+                {children}
+                <Script
+                  src={process.env.NEXT_PUBLIC_ADSTERRA_SRC}
+                  strategy="lazyOnload"
+                />
+              </ThemeProvider>
+            </MicrophoneProvider>
+          </ModalProvider>
+        </SpeakerProvider>
+      </VolumeProvider>
+    </div>
   );
 }
